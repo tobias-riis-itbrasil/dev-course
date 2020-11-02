bda0f48ca7217426ee31ca314fadfc7f0b118e76
tobias riis <odoo@ampd2.odoo.com>
2020-11-02 20:32:38 +0000

    ADD library app for exercise

---
+++ library_management/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,23 @@
+# -*- Coding: uft-8 -*-
+
+{
+    
+    'name': 'Library Managemet',
+    
+    'version': '0.1',
+    
+    'author': 'Tobias Riis',
+    
+    'description': 'Module for library management. It manages books and customers, allowing checking out books, managing rentals and more',
+    
+    'category': 'Business Administration',
+    
+    'depends': ['base'],
+    
+    'data': [],
+    
+    'application': True,
+    
+    'demo': [],
+    
+}
