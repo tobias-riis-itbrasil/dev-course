8707f7ecd84b44dffaa1af3c0a746acd053090a8
Tobias Riis <odoo@ampd2.odoo.com>
2020-11-03 12:44:05 +0000

    ADD exercise module

---
+++ odoo_academy/__init__.py
@@ -0,0 +1,3 @@
+# -*- Coding: uft-8 -*-
+
+from . import models
---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- Coding: uft-8 -*-
+
+{
+    
+    'name': 'Odoo Academy',
+    
+    'version': '0.1',
+    
+    'author': 'Tobias Riis',
+    
+    'description': 'Module for managing the Odoo Academy, including Courses, Sessions, Students and more',
+    
+    'category': 'School Administration',
+    
+    'depends': [],
+    
+    'data': [],
+    
+    'application': True,
+    
+    'demo': [],
+    
+}
